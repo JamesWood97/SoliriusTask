f2ee7351d46acdc37d67ba410070aec8a6b437eb
James <james.john.wood97@gmail.com>
2026-08-12 01:05:23 +0100

    implement CSV transformation and schema application

--- stage1/stage1.py
+++ stage1/stage1.py
@@ -2,6 +2,7 @@ import csv
 import json
 from pyspark.sql.types import StructType
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 
 def load_schema(path):
     with open(path) as file:
@@ -9,21 +10,40 @@ def load_schema(path):
 
     return StructType.fromJson(schema_dict)
 
-def load_csv(csv_path, schema, spark_session):
+def load_csv(csv_path, spark_session):
     csv_df = (
         spark_session.read
         .option("header", True)
-        .schema(schema)
+        .option("inferSchema", True)
         .csv(str(csv_path))
     )
 
     return csv_df
 
+def transform_films(df):
+    df = (df.withColumn("id", F.monotonically_increasing_id())
+            .withColumn("durationMins", F.regexp_extract("duration", r"(\d+)", 1).cast("integer"))
+            .drop("duration")
+            .withColumn(
+                "adult",
+                F.col("adult")
+            )
+            .withColumn(
+                "release_year",
+                F.make_date(F.col("release_year"), F.lit(1), F.lit(1))
+            )
+        )
+    return df
+
+def apply_schema(df, schema):
+    return df.select([F.col(field.name).cast(field.dataType).alias(field.name) for field in schema.fields])
+
 def main(spark_session):
     schema = load_schema("../resources/json/allFilesSchema.json")
-    csv_df = load_csv("../resources/csv/allFilms.csv", schema, spark_session)
-    csv_df.show()
-
+    df = load_csv("../resources/csv/allFilms.csv", spark_session)
+    df = transform_films(df)
+    df = apply_schema(df, schema)
+    df.show()
 
 if __name__ == "__main__":
     spark_session = SparkSession.builder \
